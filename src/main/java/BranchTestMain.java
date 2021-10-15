ec297223dcedb4f729be1d1d4eeab64e0cf14755
陈汉霖 <369580701@qq.com>
2021-10-15 13:55:14 +0800

    main1

---
+++ src/main/java/BranchTestMain.java
@@ -0,0 +1,10 @@
+/**
+ * @Description
+ * @Author 陈汉霖
+ * @Date 2021/10/15
+ */
+public class BranchTestMain {
+    public static void main(String[] args) {
+        System.out.println("main1");
+    }
+}
